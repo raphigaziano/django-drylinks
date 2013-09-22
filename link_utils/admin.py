622aeb1222c093582bef938a179df715b7e4212b
raphigaziano <r.gaziano@gmail.com>
2013-09-22 23:11:32 +0200

    admin default classes

---
+++ link_utils/admin.py
@@ -0,0 +1,32 @@
+"""Admin classes for the link_manager app."""
+from django.contrib import admin
+from django.utils.html import format_html
+
+
+class LinkTypeAdmin(admin.ModelAdmin):
+    list_display = ['name', 'show_icon', ]
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'icon'),
+        }),
+        ('Options', {
+            'fields': ('css_classes',),
+            'classes': ('collapse',),
+        }),
+    )
+
+    def show_icon(self, obj):
+        return format_html('<img src="%s" />' % (obj.icon.url))
+
+
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ['link_type', 'name', 'url', 'title', ]
+    fieldsets = (
+        (None, {
+            'fields': ('link_type', 'name', 'url'),
+        }),
+        ('Options', {
+            'fields': ('title', 'css_classes', 'target_blank'),
+            'classes': ('collapse',),
+        }),
+    )
