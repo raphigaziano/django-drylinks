34a659f8ca9b2532141bdc10ca0c8ae14f526c0f
raphigaziano <r.gaziano@gmail.com>
2013-09-30 21:23:36 +0200

    simpler __init__???

--- drylinks/mixins.py
+++ drylinks/mixins.py
@@ -80,9 +80,9 @@ class InternalUrlMixin(ExternalUrlMixin): # ?? URLField ok ?
 
     """
 
-    def __new__(cls, *args, **kwargs):
-        super(InternalUrlMixin, cls).__new__(*args, **kwargs)
-        cls._Meta.fields['url'].choices = (('foo', 'bar'))
+    def __init__(self, *args, **kwargs):
+        super(InternalUrlMixin, self).__init__(*args, **kwargs)
+        self.url.choices = (('foo', 'bar'))
 
     class Meta:
         abstract = True
