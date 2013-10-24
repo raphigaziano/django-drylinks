69fde6925557238486219b6c3f2417c009115c35
raphigaziano <r.gaziano@gmail.com>
2013-10-24 21:55:55 +0200

    removed link category

--- drylinks/bases.py
+++ drylinks/bases.py
@@ -11,27 +11,6 @@ from django.utils.translation import ugettext_lazy as _
 
 from . import mixins
 
-
-class AbstractLinkType(models.Model):
-    """
-    Regroup common attributes that will be shared by all links pointing
-    to the same LinkType instance.
-
-    :label: The name of this link type, i.e. 'Facebook'
-
-    """
-    label = models.CharField(
-        max_length=256,
-        verbose_name=_('Name'),
-    )
-
-    class Meta:
-        abstract = True
-
-    def __unicode__(self):
-        return self.label
-
-
 class AbstractLink(models.Model):
     """
     Abstract base class for link objects.
